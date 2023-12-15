9b171168dee0de5c3ecbd1712334972b383e55c2
Kalungi Moses <mdrewj@live.com>
2023-12-15 13:15:53 +0300

    Renderer issue report

---
+++ components/PrevNext.vue
@@ -0,0 +1,22 @@
+<script setup>
+// import icons
+// define prev and next props
+defineProps(["prev", "next"]);
+</script>
+<template>
+  <ul class="prev-next-cont">
+    <li class="link-item prev">
+      <NuxtLink v-if="prev" :to="prev._path">
+        <span> {{ prev.title }} </span>
+      </NuxtLink>
+    </li>
+    <li class="link-item next">
+      <NuxtLink v-if="next" :to="next._path">
+        <span> {{ next.title }} </span>
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+<style scoped>
+/* ... */
+</style>
--- pages/articles/[...slug].vue
+++ pages/articles/[...slug].vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div>
     <button @click="router.go(-1)">Back</button>
     <ContentRenderer :value="blogPost" />
@@ -11,4 +11,37 @@ const route = useRoute();
 const { data: blogPost } = await useAsyncData("blogPost", () =>
   queryContent("/articles").where({ _path: route.path }).findOne()
 );
+</script>-->
+
+<template>
+  <div>
+    <button @click="router.go(-1)">Back</button>
+    <PrevNext :prev="prev" :next="next" />
+    <ContentRenderer :value="data">
+      <ContentRendererMarkdown :value="data" />
+    </ContentRenderer>
+  </div>
+</template>
+
+<script setup>
+const router = useRouter();
+const route = useRoute();
+const { data } = await useAsyncData(`content-${route.path}`, async () => {
+  // fetch document where the document path matches with the cuurent route
+  let article = queryContent().where({ _path: route.path }).findOne();
+  // get the surround information,
+  // which is an array of documeents that come before and after the current document
+  let surround = queryContent()
+    .only(["_path", "title", "description"])
+    .sort({ date: 1 })
+    .findSurround(route.fullPath);
+
+  return {
+    article: await article,
+    surround: await surround,
+  };
+});
+
+// destrucure `prev` and `next` value from data
+const [prev, next] = data.value.surround;
 </script>
